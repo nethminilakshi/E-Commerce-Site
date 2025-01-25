5377e31a6181e92fc47069e9a75f405751319463
Nethu <nilakshinethmi48@gmail.com>
2025-01-25 14:48:07 +0530

    crud operations of category

---
+++ src/main/java/lk/ijse/ecommercewebsite/AddCategoryServlet.java
@@ -0,0 +1,7 @@
+package lk.ijse.ecommercewebsite;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+@WebServlet(name = "AddCategory", value = "/add-category")
+public class AddCategory extends HttpServlet {
+}
